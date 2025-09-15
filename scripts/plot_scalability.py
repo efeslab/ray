d0b02f55b91dfb0cbf796b0741a8ca6ff8f264e8
Yile Gu <guyile@amazon.com>
2025-09-14 17:20:57 -0700

    Update scalability plot script and plot

---
+++ scripts/plot_scalability.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+
+# Set global font family and size
+plt.rcParams["font.family"] = "DejaVu Sans"  # change to "Arial", "Times New Roman", etc.
+plt.rcParams["font.size"] = 16  # global font size
+
+# Data
+nodes = [1, 2, 4, 8, 16, 32]
+ray_data = [5.124827231, 10.14106279, 20.48397248, 34.56755948, 52.08752836, 66.04954455]
+ray_original = [9.219070767, 13.43074186, 17.95581079, 23.16180815, 23.3229512, 21.18512729]
+ray_original_streaming = [3.952710576, 2.961413644, 2.875978031, 3.109521308, 3.432467949, 3.5]
+
+# Plot with log scale for x-axis to give equal distance for powers of 2
+plt.figure(figsize=(5, 4))
+plt.plot(nodes, ray_data, marker='o', label="Radar")
+plt.plot(nodes, ray_original, marker='s', label="Ray")
+plt.plot(nodes, ray_original_streaming, marker='^', label="Ray Streaming")
+
+# Labels and legend
+plt.xscale("log", base=2)
+plt.xticks(nodes, nodes)
+plt.yticks([10, 20, 30, 40, 50, 60])  # enforce y-ticks
+plt.xlabel("Number of Nodes")
+plt.ylabel("Throughput (GB/s)")
+# plt.title("Throughput vs Number of Clusters (Log Scale X-axis)")
+plt.legend()
+plt.grid(True, linestyle='--', alpha=0.6)
+plt.tight_layout()
+
+plt.savefig("scalability_plot.pdf")
